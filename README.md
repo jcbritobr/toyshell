8e8a209a7d1c86be8755229963d6e671eae25054
jcbritobr <jcbritobr@gmail.com>
2022-01-29 13:02:24 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Toy Shell Written in Rust
+This is a simple toy shell written in Rust language with porpose of learning system programmim, and in a specific theme, process management.
+
+* Compile
+```
+$ cargo build
+```
+
+* Run
+```
+jcbritobr@DESKTOP-DT4KS9S:~/Workspace/rust/toyshell$ cargo run
+    Finished dev [unoptimized + debuginfo] target(s) in 0.02s
+     Running `target/debug/toyshell`
+>> ls -lht
+total 20K
+drwxr-xr-x 2 jcbritobr jcbritobr 4.0K Jan 29 12:55 src
+-rw-r--r-- 1 jcbritobr jcbritobr  224 Jan 29 12:55 README.md
+drwxr-xr-x 3 jcbritobr jcbritobr 4.0K Jan 29 12:40 target
+-rw-r--r-- 1 jcbritobr jcbritobr  177 Jan 29 12:38 Cargo.toml
+-rw-r--r-- 1 jcbritobr jcbritobr  152 Jan 29 11:03 Cargo.lock
+>>
+```
+
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{
+    io::{stdin, stdout, Write},
+    process::Command,
+};
+
+fn main() {
+    loop {
+        print!(">> ");
+        stdout().flush().expect("Cant flush stdout buffer");
+        let mut user_input = String::new();
+        stdin()
+            .read_line(&mut &mut user_input)
+            .expect("Unable to read user input");
+
+        let command_to_execute = user_input.trim();
+        let command_args: Vec<&str> = command_to_execute.split_whitespace().collect();
+
+        if command_args.len() <= 0 {
+            continue;
+        }
+
+        let mut child = Command::new(command_args[0])
+            .args(&command_args[1..])
+            .spawn()
+            .expect("Unable to execute command");
+
+        child.wait().expect("Abnormal process termination");
+    }
+}
