3e17a17587c4ca9f13de74593e68b854bf74eebc
jcbritobr <jcbritobr@gmail.com>
2022-01-29 13:55:08 -0300

    Fixed double mut reference.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         stdout().flush().expect("Cant flush stdout buffer");
         let mut user_input = String::new();
         stdin()
-            .read_line(&mut &mut user_input)
+            .read_line(&mut user_input)
             .expect("Unable to read user input");
 
         let command_to_execute = user_input.trim();
